d1c70bed2e163195fe59dbdb9fad57885a7027a9
Volkivska <vitalina.v@air-dr.com>
2025-08-11 09:53:29 +0300

    countTheMonkeys implementation

--- src/main/kotlin/mate/academy/CountTheMonkeys.kt
+++ src/main/kotlin/mate/academy/CountTheMonkeys.kt
@@ -3,5 +3,11 @@ package mate.academy
 // Implement the `countTheMonkeys()` function so that it returns an IntArray
 // with elements from `1` to `count`.
 fun countTheMonkeys(count: Int): IntArray {
-
+    val list : MutableList<Int> = mutableListOf()
+    if (count == 0) return intArrayOf() else
+    for (i in 1..count) {
+        list.add(i)
+    }
+    return list.toIntArray()
 }
+
